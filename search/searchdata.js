cecb9678ccf6107a27fae2b4a86cd2ed820d5ba2
ArisenAlcoholic <ArisenAlcoholic@users.noreply.github.com>
2026-01-30 18:35:52 +0000

    Deploying to gh-pages from @ ArisenAlcoholic/420chan@26736543dbd4fdb33e2412c0005c9ad28e6faaa3 🚀

---
+++ search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "4",
+  1: "4"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "Todos",
+  1: "Páginas"
+};
+
